21d2337bffc3f5bc6432f4d4d4f50b9a45a77d51
Arihy <ynallem@gmail.com>
2013-03-12 14:43:46 +0100

    Ajout de la méthode traceRoute

--- src/roadGenerator/RoadGenerator.java
+++ src/roadGenerator/RoadGenerator.java
@@ -1,5 +1,24 @@
 package roadGenerator;
 
-public class RoadGenerator {
+import map.Map;
+import map.exceptions.BadStateException;
 
+
+public class RoadGenerator {
+	public static void traceRoute(int x1, int z1, int x2, int z2, int blockY, Map map) throws BadStateException
+	{
+		int x, y;
+		double a, b;
+		
+		a = (double) (z2 - z1) / (x2 - x1);
+		b = z1 - a * x1;
+		
+		for(x = x1; x <= x2; x++)
+		{
+			y = (int) (a * x + b);
+			//tester si la valeur du block voisin n'est pas pas null (ou air)
+			//alr placer un block.
+			map.setBlock(x, y, blockY, (short) 49);
+		}
+	}
 }

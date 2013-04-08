69a7f774bbf5381abc67920a350af578b092f95e
Pholoris <pholoris@gmail.com>
2013-04-08 15:49:38 +0200

    Modification classe Batiment

--- src/openstreetcraft/buildingGenerator/Batiment.java
+++ src/openstreetcraft/buildingGenerator/Batiment.java
@@ -1,8 +1,8 @@
 package openstreetcraft.buildingGenerator;
 
 import java.util.Vector;
-import map.Map;
 import openstreetcraft.Location;
+import map.Map;
 
 public class Batiment {
 	
@@ -18,7 +18,7 @@ public class Batiment {
 		this.matiereExt=98;
 		this.matiereInt=5;
 		Location d=this.pointDepart(), f=this.pointFinal();
-		this.hauteur = 4 + (int)(((f.getX()-d.getX()+1)*(f.getZ()-d.getZ()+1)) *(double)(Math.random()+1));
+		this.hauteur = 4 + (int)(((f.getX()-d.getX()+1)*(f.getZ()-d.getZ()+1)/25) *(double)(Math.random()+1));
 		System.out.println(this.hauteur);
 		struct = new Structure(f.getX()-d.getX()+1,getHauteur(),f.getZ()-d.getZ()+1);
 		this.dessinerStructure();
@@ -46,67 +46,57 @@ public class Batiment {
 	private void dessinerMur(Location pDep, Location pFin, boolean placerPorte){
 		Location d=this.pointDepart(), p1, p2;
 		double coef;
-		int x, z, curX, curY, curZ, distX=Math.abs(pFin.getX()-pDep.getX())+1, distZ=Math.abs(pFin.getZ()-pDep.getZ())+1;
+		int Larg, Long, curX, curY, curZ, distance, curLong, longueur;
+		int distX=Math.abs(pFin.getX()-pDep.getX())+1, distZ=Math.abs(pFin.getZ()-pDep.getZ())+1;
+		boolean longZ=(distZ>distX);
 		
-		if( distZ > distX ){
+		if( longZ ){
 			if(pDep.getZ()<pFin.getZ()){p1=pDep;p2=pFin;}
-			else{p2=pDep;p1=pFin;}
-			
+			else{p2=pDep;p1=pFin;}			
 			coef=(double)(p2.getX()-p1.getX())/(double)(distZ);
-			for(z=0;z<distZ;z++){
-				x=(int)(coef*z);if(coef>0)x++;
-				for(int y=0;y<this.getHauteur();y++){
-					curX=p1.getX()+x-d.getX(); curZ=p1.getZ()+z-d.getZ(); curY=p1.getY()+y-d.getY();
-					if(curX<struct.getTailleX() && curZ<struct.getTailleZ() && curY<struct.getTailleY()){
-						if(placerPorte && coef==0 && (curY==1||curY==2) && curZ==(struct.getTailleZ()/2)){
-							struct.setBlock(curX, curZ, curY, (short)64);
-						}
-						else if(curZ<(struct.getTailleZ()-1) && (curZ%3)==2
-								&& curY<(struct.getTailleY()-1) && (curY%5)>1 && (curY%5)<4){
-							if(coef==0){
-								struct.setBlock(curX, curZ, curY, (short)102);
-							}
-							else{
-								struct.setBlock(curX, curZ, curY, (short)20);
-							}
-						}
-						else{
-							struct.setBlock(curX, curZ, curY, this.matiereExt);
-						}
-					}
-				}
-			}
-		}
+			longueur=distZ;
+		}	
 		else{
 			if(pDep.getX()<pFin.getX()){p1=pDep;p2=pFin;}
-			else{p2=pDep;p1=pFin;}
-			
+			else{p2=pDep;p1=pFin;}			
 			coef=(double)(p2.getZ()-p1.getZ())/(double)(distX);
-			for(x=0;x<distX;x++){
-				z=(int)(coef*x);if(coef>0)z++;
-				for(int y=0;y<this.getHauteur();y++){
-					curX=p1.getX()+x-d.getX(); curZ=p1.getZ()+z-d.getZ(); curY=p1.getY()+y-d.getY();
-					if(curX<struct.getTailleX() && curZ<struct.getTailleZ() && curY<struct.getTailleY()){
-						if(placerPorte && coef==0 && (curY==1||curY==2) && curX==(struct.getTailleX()/2)){
-							struct.setBlock(curX, curZ, curY, (short)64);
+			longueur=distX;
+		}
+		
+		for (Long = 0; Long < longueur; Long++) {
+			Larg = (int) (coef * Long);
+			if (coef > 0)
+				Larg++;
+			for (int y = 0; y < this.getHauteur(); y++) {
+				if(longZ){
+					curX = p1.getX() + Larg - d.getX();
+					curZ = p1.getZ() + Long - d.getZ();
+					distance=struct.getTailleZ();curLong=curZ;
+				}else{
+					curX = p1.getX() + Long - d.getX();
+					curZ = p1.getZ() + Larg - d.getZ();
+					distance=struct.getTailleX();curLong=curX;
+				}
+				curY = p1.getY() + y - d.getY();
+				if (curX < struct.getTailleX() && curZ < struct.getTailleZ() && curY < struct.getTailleY()) {
+					if (placerPorte && coef == 0 && (curY==1 || curY==2) && curLong==(distance/2)) {
+						if(curY==1){struct.setBlock(curX, curZ, curY, (short)(0*256+64));}
+						else if(curY==2){struct.setBlock(curX, curZ, curY, (short)(8*256+64));}
+					} else if (curLong < (distance - 1) && (curLong % 3) == 2
+							&& curY < (struct.getTailleY() - 1) && (curY % 5) > 1 && (curY % 5) < 4) {
+						if (coef == 0) {
+							struct.setBlock(curX, curZ, curY, (short) 102);
+						} else {
+							struct.setBlock(curX, curZ, curY, (short) 20);
 						}
-						else if(curX<(struct.getTailleX()-1) && (curX%3)==2
-								&& curY<(struct.getTailleY()-2) && (curY%5)>1 && (curY%5)<4){
-							if(coef==0){
-								struct.setBlock(curX, curZ, curY, (short)102);
-							}
-							else{
-								struct.setBlock(curX, curZ, curY, (short)20);
-							}
-						}
-						else{
-							struct.setBlock(curX, curZ, curY, this.matiereExt);
-						}
+					} else {
+						struct.setBlock(curX, curZ, curY, this.matiereExt);
 					}
 				}
 			}
 		}
 	}
+	
 	
 	private void dessinerSol(){
 		for(int x=0;x<struct.getTailleX();x++){
